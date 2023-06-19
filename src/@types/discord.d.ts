39d203173fbf8644ba803fbbd78899dcb4141061
nikko <35370166+nikkoxd@users.noreply.github.com>
2023-06-19 11:36:14 +0400

    Revert "Implement cooldowns"
    This reverts commit c24b2ce8d9cc3eae1c63603b991bdc8b119f31c5.

--- src/@types/discord.d.ts
+++ src/@types/discord.d.ts
@@ -3,6 +3,5 @@ import { Collection } from "discord.js";
 declare module "discord.js" {
   export interface Client {
     commands: Collection<any, any>;
-    cooldowns: Collection<any, any>;
   }
 }
